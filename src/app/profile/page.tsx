c648e2b6f4158a8b2d7e3eb5e0ed5f95f384f848
Pol Hidalgo <80904366+Polqt@users.noreply.github.com>
2025-01-19 18:32:28 +0800

    Add user profile page and API for fetching user details; implement middleware for authentication

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,11 @@ import axios from 'axios'
 import Link from 'next/link'
 import { toast } from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function ProfilePage() {
     const router = useRouter()
+    const [user, setUser] = useState('Nothing')
 
     const logout = async () => {
         try {
@@ -17,16 +19,31 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/mine')
+        setUser(res.data.data._id)
+    }
+
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <h1>Profile</h1>
             <p>Profile Page</p>
+            <h2 className='m-4 p-3 rounded bg-green-800'>
+                {user === 'Nothing' ? 'Nothing' : <Link href={`/profile/${user}`}>{user}</Link>}
+            </h2>
 
             <button 
                 type='submit'
                 onClick={logout}
                 className="bg-blue-500 mt-4 hover:bg-blue-700 text-white py-2 px-4 rounded"
             >Logout
+            </button>
+
+            <button 
+                type='submit'
+                onClick={getUserDetails}
+                className="bg-blue-500 mt-4 hover:bg-blue-700 text-white py-2 px-4 rounded"
+            >Get User Details
             </button>
         </div>
     )
